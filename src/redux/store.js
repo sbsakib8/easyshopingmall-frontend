068dd1bcc05b5d94b9d430f825b637964d8c1756
sakib <sbsakibsarkar8@gmail.com>
2025-10-07 06:27:47 -0700

    subcategori edit

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import productReducer from "./productSlice";
 import categoryReducer from "./categorySlice"
+import  subcategoryRoute  from "./subcategorySlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     product: productReducer,
-    category: categoryReducer
+    category: categoryReducer,
+    subcategory:subcategoryRoute
 
   },
 });
