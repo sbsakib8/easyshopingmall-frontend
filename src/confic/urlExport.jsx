4c6f24c595fbb7f618a2aeea848943230b9c5c84
sakib <sbsakibsarkar8@gmail.com>
2025-12-06 00:16:22 -0800

    deploy

--- src/confic/urlExport.jsx
+++ src/confic/urlExport.jsx
@@ -1,2 +1,5 @@
-export const UrlFrontend = "https://easyshoppingmallbd.com/"
-export const UrlBackend = "https://api.easyshoppingmallbd.com/"
+// export const UrlFrontend = "https://easyshoppingmallbd.com/"
+// export const UrlBackend = "https://api.easyshoppingmallbd.com/"
+
+export const UrlFrontend = process.env.NEXT_PUBLIC_FRONTEND_URL;
+export const UrlBackend = process.env.NEXT_PUBLIC_BACKEND_URL;
