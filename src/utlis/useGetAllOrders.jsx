476ec598e73f79ee8bf70422d35585dc712da505
oliullahakib <oliullahakib@gmail.com>
2026-01-10 23:46:38 +0600

    added old code

---
+++ src/utlis/useGetAllOrders.jsx
@@ -0,0 +1,29 @@
+"use client";
+import { useEffect, useState, useCallback } from "react";
+import { OrderAllGetAdmin } from "./useOrder";
+
+// ✅ Custom hook
+export const useGetAllOrders = () => {
+  const [allOrders, setAllOrders] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchGetAllOrders = useCallback(async () => {
+    try {
+      setLoading(true);
+      const data = await OrderAllGetAdmin();
+      setAllOrders(data.data);
+      setError(null);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchGetAllOrders();
+  }, [fetchGetAllOrders]);
+
+  return { allOrders, loading, error, refetch: fetchGetAllOrders };
+};
