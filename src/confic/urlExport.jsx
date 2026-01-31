631a707dbaece46a6002b50e107e55c066915e86
oliullahakib <oliullahakib@gmail.com>
2026-02-01 00:53:44 +0600

    mergin data

--- src/confic/urlExport.jsx
+++ src/confic/urlExport.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // export const UrlFrontend = "https://easyshoppingmallbd.com"
 export const UrlBackend = "http://localhost:5001/api"
 export const UrlFrontend = "http://localhost:3000"
@@ -6,7 +5,3 @@ export const UrlFrontend = "http://localhost:3000"
 
 // export const UrlFrontend = process.env.NEXT_PUBLIC_FRONTEND_URL;
 // export const UrlBackend = process.env.NEXT_PUBLIC_API_BASE_URL;
-=======
-export const UrlFrontend = process.env.NEXT_PUBLIC_FRONTEND_URL;
-export const UrlBackend = process.env.NEXT_PUBLIC_API_BASE_URL;
->>>>>>> cfac250ade498a7e2863567a85e1a2e1a18c0424
