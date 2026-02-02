c032ca6304b17833d5d3edf3fa3bc0924e79171d
oliullahakib <oliullahakib@gmail.com>
2026-02-03 00:29:43 +0600

    added colour theme and bg to popular section

--- src/compronent/Home/popularProducts.jsx
+++ src/compronent/Home/popularProducts.jsx
@@ -370,8 +370,8 @@ const PopularProducts = ({ initialData }) => {
       </div>
 
       {/* Product Grid */}
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 sm:gap-6">
+      <div className=" bg-base-300 ">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 sm:gap-6 container mx-auto px-3 sm:px-6 lg:px-8 py-8">
           {filteredProducts.map((product) => (
             <Link
               href={`/productdetails/${product.id}`}
