3d4488ef91ab9e8ad99385ba870e12a5eb55c197
Md. Rakibul Islam <rakibul00206@gmail.com>
2026-05-09 20:18:11 +0600

    loading state added

--- src/confic/urlExport.jsx
+++ src/confic/urlExport.jsx
@@ -1,5 +1,5 @@
 // export const UrlFrontend = "https://easyshoppingmallbd.com"
-// export const UrlBackend = "http://localhost:5001/api"
+// export const UrlBackend = "http://localhost:5004/api"
 // export const UrlFrontend = "http://localhost:3000"
 
 export const UrlFrontend = process.env.NEXT_PUBLIC_FRONTEND_URL;
