2ee30ca373cca6379b936c3aa7081a36db1b1210
Md. Rakibul Islam <rakibul00206@gmail.com>
2026-05-17 17:51:50 +0600

    Refactor product loading and display components; implement SubCategoryCardSection for improved structure and user experience

---
+++ app/(dropShipping)/boost-products/loading.jsx
@@ -0,0 +1,7 @@
+import { SubCategoryCardSectionLoading } from "@/src/dropShipping/SubCategoryCardSection/SubCategoryCardSection";
+
+const loading = () => {
+  return <SubCategoryCardSectionLoading />;
+};
+
+export default loading;
